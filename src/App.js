2406d23b2a6363b53c8033fa17ce4d2bb28ff731
yeojis <ji.yeo@infranicsmail.com>
2022-02-08 18:00:26 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
+{
+  /* Javascript 샘플 코드 */
+
+
+var xhr = new XMLHttpRequest();
+var url = "http://apis.data.go.kr/9760000/PofelcddInfoInqireService/getPoelpcddRegistSttusInfoInqire"; /*URL*/
+var queryParams = "?" + encodeURIComponent("serviceKey") + "="+process.env.REACT_APP_API_KEY; /*Service Key*/
+queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1');
+queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('100'); 
+queryParams += '&' + encodeURIComponent('sgId') + '=' + encodeURIComponent('20220309');/*선거ID*/
+queryParams += '&' + encodeURIComponent('sgTypecode') + '=' + encodeURIComponent('1'); /*선거종류코드*/
+queryParams += '&' + encodeURIComponent('sggName') + '=' + encodeURIComponent(''); /*선거구명*/
+queryParams += '&' + encodeURIComponent('sdName') + '=' + encodeURIComponent('');  /*시도명*/
+xhr.open('GET', url + queryParams);
+xhr.onreadystatechange = function () {
+    if (this.readyState == 4) {
+        alert('Status: '+this.status+'nHeaders: '+JSON.stringify(this.getAllResponseHeaders())+'nBody: '+this.responseText);
+    }
+};
+
+xhr.send('');
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          나이스
         </p>
         <a
           className="App-link"
