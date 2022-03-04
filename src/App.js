c7e24e067642d41d609b1a71520c7fff8641b261
yeojis <ji.yeo@infranicsmail.com>
2022-03-04 17:49:58 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,44 +1,48 @@
 import logo from './logo.svg';
+import axios from "axios";
+import React,{useEffect} from 'react';
+import XMLParser from 'react-xml-parser';
 import './App.css';
-
-{
-  /* Javascript 샘플 코드 */
 
 
 var xhr = new XMLHttpRequest();
-var url = "http://apis.data.go.kr/9760000/PofelcddInfoInqireService/getPoelpcddRegistSttusInfoInqire"; /*URL*/
+var url = "/9760000/PofelcddInfoInqireService/getPofelcddRegistSttusInfoInqire"; /*URL*/
 var queryParams = "?" + encodeURIComponent("serviceKey") + "="+process.env.REACT_APP_API_KEY; /*Service Key*/
-queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1');
-queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('100'); 
-queryParams += '&' + encodeURIComponent('sgId') + '=' + encodeURIComponent('20220309');/*선거ID*/
-queryParams += '&' + encodeURIComponent('sgTypecode') + '=' + encodeURIComponent('1'); /*선거종류코드*/
-queryParams += '&' + encodeURIComponent('sggName') + '=' + encodeURIComponent(''); /*선거구명*/
-queryParams += '&' + encodeURIComponent('sdName') + '=' + encodeURIComponent('');  /*시도명*/
-xhr.open('GET', url + queryParams);
-xhr.onreadystatechange = function () {
-    if (this.readyState == 4) {
-        alert('Status: '+this.status+'nHeaders: '+JSON.stringify(this.getAllResponseHeaders())+'nBody: '+this.responseText);
-    }
-};
+queryParams += '&pageNo=1';
+queryParams += '&numOfRows=100'; 
+queryParams += '&sgId=20220309';/*선거ID*/
+queryParams += '&sgTypecode=1'; /*선거종류코드*/
+queryParams += '&sggName='; /*선거구명*/
+queryParams += '&sdName=';  /*시도명*/
 
-xhr.send('');
-}
+// xhr.open('GET', url + queryParams);
+// xhr.onreadystatechange = function () {
+//     if (this.readyState == 4) {
+//         console.log('Status: '+this.status+'nHeaders: '+JSON.stringify(this.getAllResponseHeaders())+'nBody: '+this.responseText);
+//     }
+// };
+
+// xhr.send('');
+// }
 
 function App() {
+
+  useEffect(()=>{
+    testData();
+  },[]);
+    
+  const testData = async()=> {
+    axios.get(url+queryParams).then((res)=>
+    {
+      var xml = new XMLParser().parseFromString(res.data); 
+      console.log(xml.children[1]);
+    }) .catch(function(error) {
+      console.log("에러");
+  });
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
